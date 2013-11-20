fff42409c2d9620d815e3336e1ceb839e0c12a6e
Moin Virani <moin.virani@gmail.com>
2013-11-20 17:56:37 -0500

    completed excersises

--- example.js
+++ example.js
@@ -37,7 +37,7 @@ document.write(lifeSupply("23", "100", "4"));
 document.write("</br><br>");
 
 
-// The Geometrizer
+// The Geometrizer (Exc #4)
 function circleProp(radius) {
   var circumference = "The circumference is " + ((3.142)*(radius*2))
   return circumference;
@@ -46,7 +46,8 @@ function circleProp(radius) {
 document.write(circleProp("4"));
 document.write("</br><br>");
 
-// The Temperature Converter
+
+// The Temperature Converter (Exc #5)
 function tempConverter(celcius) {
   var convert = celcius + "C" + " is " + (celcius*(9/5)+32) + "F"
   return convert
@@ -56,85 +57,5 @@ document.write(tempConverter("1"));
 document.write("</br><br>");
 
 
-/*
 
-//  Learing Functions
-
-var myFunction = function(greeting) {
-  return greeting;
-};
-
-var x = myFunction ("hello!");
-console.log(x);
-*/
-
-/*// Do While loop
-var num = 1;
-do {
-  num++;
-  console.log(num); 
-} while (num < 10);
-*/
-
-/*
-// for loop
-for (var i = 0; i <= 20; i++) {
-  var evenOrOdd;
-  if (i % 2 === 0) {
-    evenOrOdd = "even";
-  } else {
-    evenOrOdd = "odd";
-  }
-  console.log(i + "is " + evenOrOdd);
-};*/
-
-/*
- // Excersize 1
-var time = 12;
-var x;
-
-if (time < 11) {
-  x = "Good morning!";
-} else if (time < 20) {
-  x = "Good day!";
-}  
-else {
-  x = "Good evening!";  
-};
-
-document.write("<h1>" + x + "</h1>");
-*/
-
-/*
- // Excersize 2
-var day = new Date().getDay();
-var dayOfTheWeek;
-switch (day) {
-  case 0:
-   dayOfTheWeek ="Today is Sunday";
-    break;    // break stops execution without giving a value
-     case 1:
-   dayOfTheWeek ="Today is Monday";
-    break;
-     case 2:
-   dayOfTheWeek ="Today is Tuesday";
-    break;
-     case 3:
-   dayOfTheWeek ="Today is Wednesday";
-    break;
-     case 4:
-   dayOfTheWeek ="Today is Thursday";
-    break;
-     case 5:
-   dayOfTheWeek ="Today is Friday";
-    break
-     case 6:
-   dayOfTheWeek ="Today is Saturday";
-    break;
-
-};
-
-document.write("<h1>Today is " + dayOfTheWeek + ".</h1>")
-
-*/
 
